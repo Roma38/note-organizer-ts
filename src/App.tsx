bdfb3b49b3f144ff40f50d396be9af6c24fb0c63
Roma Kutishchev <rukkola2@gmail.com>
2021-04-12 21:48:01 +0300

    add initial data

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,18 @@ import { FAKE_API as axios } from "./fakeAPI";
 import { HeaderComponent as Header } from "./components/Header";
 import { HomePage } from "./components/HomePage";
 import { notesLoadSucceed } from "./store/actions/notes";
+import { initNotes, initTags } from "./constants";
 
 function App() {
   const dispatch = useDispatch();
   const [isNotesLoading, setIsNotesLoading] = useState(false);
 
   useEffect(() => {
+    if (!localStorage.getItem('tags') && !localStorage.getItem('notes')) {
+      localStorage.setItem('tags', JSON.stringify(initTags));
+      localStorage.setItem('notes', JSON.stringify(initNotes));
+      console.log("Init data")
+    }
     setIsNotesLoading(true);
     axios.get("notes")
       .then(({ data }) => {
@@ -33,7 +39,7 @@ function App() {
         </Dimmer> :
         <>
           <Header />
-          <HomePage/>
+          <HomePage />
         </>}
 
     </Container>
--- src/constants.ts
+++ src/constants.ts
@@ -14,3 +14,100 @@ export const COLORS = [
   "grey",
   "black"
 ];
+
+export const initNotes = [
+  {
+    "id": "0bd314be-af3c-45b9-a87a-3740b3b744c1",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Justo nec ultrices dui sapien.",
+    "tags": [
+      "34bf1743-9320-4a43-8415-e54986aead0b",
+      "54456351-5737-4f03-a430-6c3cac83a63e"
+    ],
+    "isPinned": false
+  },
+  {
+    "id": "1f064afb-a936-4f59-95db-da07e3318393",
+    "content": "Sodales ut eu sem integer vitae. Morbi quis commodo odio aenean sed.",
+    "tags": [
+      "3cd31c32-867d-4be8-a7bf-429bd25fded0",
+      "bf21dab8-7674-4e40-8f46-11fc43caef0d",
+      "6f41f3c6-531a-43a4-8aee-b00d3e642e39"
+    ],
+    "isPinned": false
+  },
+  {
+    "id": "3825dc54-b7cf-4979-b139-0c42a9741e51",
+    "content": "Dignissim convallis aenean et tortor at risus viverra adipiscing at. Cursus turpis massa tincidunt dui ut ornare lectus sit amet. Elit sed vulputate mi sit.",
+    "tags": [
+      "834a3a03-2773-4d71-864d-e34c3aba5d94"
+    ],
+    "isPinned": false
+  },
+  {
+    "id": "f986b772-0b61-40a2-b565-af7b1651203a",
+    "content": "Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta lorem mollis aliquam ut. Non tellus orci ac auctor augue mauris augue neque. Sed risus pretium quam vulputate dignissim suspendisse in est.",
+    "tags": [
+      "bdb3f481-3334-472a-8887-56ae48f057c4"
+    ],
+    "isPinned": true
+  },
+  {
+    "id": "0071f098-08dc-4ac6-8dc5-22e5000bee77",
+    "content": "Tincidunt arcu non sodales neque sodales ut etiam. At consectetur lorem donec massa sapien faucibus et molestie. Ac placerat vestibulum lectus mauris ultrices eros in cursus",
+    "tags": [
+      "bdb3f481-3334-472a-8887-56ae48f057c4",
+      "834a3a03-2773-4d71-864d-e34c3aba5d94",
+      "34bf1743-9320-4a43-8415-e54986aead0b"
+    ],
+    "isPinned": false
+  },
+  {
+    "id": "e87e3c77-7fc7-4f32-a9f7-b4dcd60b8236",
+    "content": "Purus faucibus ornare suspendisse sed. Quam pellentesque nec nam aliquam sem.",
+    "tags": [
+      "834a3a03-2773-4d71-864d-e34c3aba5d94",
+      "bdb3f481-3334-472a-8887-56ae48f057c4",
+      "54456351-5737-4f03-a430-6c3cac83a63e"
+    ],
+    "isPinned": false
+  },
+  {
+    "id": "f62e2aaf-40ab-4bfb-b7f4-5dcfbf87ff19",
+    "content": "Lectus magna fringilla urna porttitor rhoncus dolor purus non.",
+    "tags": [],
+    "isPinned": false
+  },
+  {
+    "id": "28cedc0b-2a1c-415c-8256-5ec72882230e",
+    "content": "Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. ",
+    "tags": [
+      "bf21dab8-7674-4e40-8f46-11fc43caef0d",
+      "6f41f3c6-531a-43a4-8aee-b00d3e642e39",
+      "dd7527cc-1403-44d5-8599-04bd7862447f",
+      "834a3a03-2773-4d71-864d-e34c3aba5d94"
+    ],
+    "isPinned": false
+  }
+]
+export const initTags = [
+  {
+    "id": "bdb3f481-3334-472a-8887-56ae48f057c4",
+    "name": "todo"
+  },
+  {
+    "id": "834a3a03-2773-4d71-864d-e34c3aba5d94",
+    "name": "done"
+  },
+  {
+    "id": "34bf1743-9320-4a43-8415-e54986aead0b",
+    "name": "important"
+  },
+  {
+    "id": "54456351-5737-4f03-a430-6c3cac83a63e",
+    "name": "note"
+  },
+  {
+    "id": "dd7527cc-1403-44d5-8599-04bd7862447f",
+    "name": "ASAP"
+  }
+]
